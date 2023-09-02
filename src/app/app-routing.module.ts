33bec47172701a5392dd6fcfc4040d1303f4c53a
RAVINDULM <ravindu.kp@gmail.com>
2023-09-02 20:42:52 +0530

    Add v1 codes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'services',component:ServicesComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact', component:ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
